ca0546c7d14ceb608544eb4ac85ef2e2ae4b17d6
Saurabh <saurabh1258.be21@chitkarauniversity.edu.in>
2022-06-25 14:05:49 +0530

    json added

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 // ignore: unused_import
 import 'login_page.dart';
+import 'dart:convert';
 
 // ignore: camel_case_types
-class homePage extends StatelessWidget {
+class homePage extends StatefulWidget {
   const homePage({Key? key}) : super(key: key);
 
-  
-
   @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+
+  
+  @override
+  void initState(){
+    super.initState();
+    loadData();
+  }
+  loadData()async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedJson = jsonDecode(catalogJson);
+    var productData = jsonDecode(decodedJson);
+  }
   Widget build(BuildContext context) {
     final dummy_list = List.generate(25, (index) => CatalogModel.items[0]);
     return Scaffold(
