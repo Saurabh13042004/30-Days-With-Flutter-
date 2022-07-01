fe30e30d81a7decffdbef217df1883dc40eff997
Saurabh <saurabh1258.be21@chitkarauniversity.edu.in>
2022-07-01 16:10:17 +0530

    gridview

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -45,14 +45,52 @@ class _homePageState extends State<homePage> {
         title:  const Text("Catalog App")),
         body: Padding(
           padding: const EdgeInsets.all(18.0),
-          child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty) ?  ListView.builder(
-            itemCount: CatalogModel.items.length,
+          child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)
+          ?GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16
+              ), 
             itemBuilder: (context,index){
-              return ItemWidget(
-                item:CatalogModel.items[index]
+               final item = CatalogModel.items[index];
+              return Card(
+
+                clipBehavior: Clip.antiAlias,
+               shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8)
+               ),
+               child:GridTile(
+                header: Container
+                (child: Text(item.name,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                ),
+                padding: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+
+                ),
+                ),
+                 child:Image.network(item.image),
+                 footer: Container
+                (child: Text(item.price.toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                ),
+                padding: EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+
+                ),
+                ),
+                ),
                 );
             },
-          )
+            itemCount: CatalogModel.items.length,
+            ) 
           :Center(
 
             child:CircularProgressIndicator(),
